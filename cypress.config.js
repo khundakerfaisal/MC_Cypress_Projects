a946f9db3b9b985d0c77840f2334eb8f337d8ae3
khundakerfaisal <khundakermis@gmail.com>
2025-07-08 18:20:51 +0600

    added json read write file with correct format

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    defaultCommandTimeout: 10000,
-    pageLoadTimeout: 60000, 
     setupNodeEvents(on, config) {
       config.specPattern = [
         'cypress/e2e/MCTestRunner/login.cy.js',
